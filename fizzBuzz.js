cbf51a6a00f406b2fdc3f5c5c828343b63a4615c
Jeremy-Moss <mosstoggle@gmail.com>
2022-08-26 01:24:20 -0400

    Finished

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,12 @@
 // Write your solution below this line:
+for(let i = 0; i <= 50; i++) {
+    if((i % 3 === 0) && (i % 5 === 0)) {
+        console.log('Fizzbuzz')
+    } else if(i % 3 === 0) {
+        console.log('Fizz')
+    } else if(i % 5 === 0) {
+        console.log('buzz')
+    } else {
+        console.log(i)
+    }
+}
